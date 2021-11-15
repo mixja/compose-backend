fc29a4e17083c0d5594cca31e0e584e5102b4510
Justin Menga <justin.menga@gmail.com>
2021-11-15 13:09:55 -0800

    AUpdate subscriptions when connection disconnects

--- src/disconnect.ts
+++ src/disconnect.ts
@@ -3,35 +3,43 @@ import { DynamoDBDocument } from "@aws-sdk/lib-dynamodb";
 
 const dynamodb = new DynamoDB({});
 const client = DynamoDBDocument.from(dynamodb);
-const { DYNAMODB_TABLE } = process.env;
+const { DYNAMODB_TABLE, DYNAMODB_GSI1_INDEX } = process.env;
+
+// TODO: implement TypeScript equivalent of extension method below
+Object.defineProperty(Array.prototype, 'chunk', {
+  value: function(chunkSize) {
+      var that = this;
+      return Array(Math.ceil(that.length/chunkSize)).fill().map(function(_,i){
+          return that.slice(i*chunkSize,i*chunkSize+chunkSize);
+      });
+  }
+});
 
 export const handler = async (event, context?) => {
   const { connectionId } = event.requestContext;
 
-  // TODO - do this in a transaction instead
+  // TODO: handle pagination
   try {
-    await Promise.all([
-      client.update({
-        TableName: DYNAMODB_TABLE,
-        Key: {
-          pk: `TODO - what goes here?`,
-          sk: `connection#${connectionId}#subscription`,
-        },
-        ConditionExpression: "attribute_not_exists(unsubscribedAt)", // should I add a GSI on unsubscribedAt?
-        UpdateExpression: "SET unsubscribedAt = :t",
-        ExpressionAttributeValues: { ":t": Date.now() },
-      }),
-      client.update({
-        TableName: DYNAMODB_TABLE,
-        Key: {
-          pk: `connection#${connectionId}`,
-          sk: `connection#${connectionId}`,
-        },
-        UpdateExpression: "SET disconnectedAt = :t",
-        ExpressionAttributeValues: { ":t": Date.now() },
-      }),
-    ]);
-    return { statusCode: 200 };
+    const { Items: items } = await client.query({
+      TableName: DYNAMODB_TABLE,
+      IndexName: DYNAMODB_GSI1_INDEX,
+      KeyConditionExpression: "gsi1pk = :gsi1pk AND begins_with(gsi1sk, :gsi1sk)",
+      ExpressionAttributeValues: {
+        ":gsi1pk": `connection#${connectionId}`,
+        ":gsi1sk": `status#connected`
+      }
+    });
+    await Promise.all(items.chunk(25).map(chunk => {
+      client.batchWrite({
+        RequestItems: {
+          [DYNAMODB_TABLE]: chunk.map(c => ({
+            PutRequest: {
+              Item: { ...c, gsi1sk: "status#disconnected#" + Date.now() }
+            }
+          }))
+        }
+      })
+    }));
   } catch (e) {
     console.log(e);
     return { statusCode: 500 };
